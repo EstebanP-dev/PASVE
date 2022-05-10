2a250545745f9e0afaac3474ccbd3cc904c4ba86
Esteban Navia <developer2@incloft.com.co>
2022-05-10 17:05:50 -0500

    implement api with users services

---
+++ Controllers/ApiEvidenceController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using PASVE.Interfaces;
+using PASVE.Models;
+using PASVE.Services;
+using System.Web.Http;
+
+namespace PASVE.Controllers
+{
+    public class ApiEvidenceController : ApiController, IGenericApiController<Evidence, Guid>
+    {
+        private readonly EvidenceService service = new EvidenceService();
+
+        [Route("api/Evidence/{id}")]
+        public Task<BaseResponse<Evidence>> ConsultAsync([FromUri] Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("api/Evidence/{id}/Delete")]
+        public Task<BaseResponse<Evidence>> DeleteAsync([FromUri] Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("api/Evidence")]
+        public Task<BaseResponse<List<Evidence>>> GetAllAsync(int page, int rows)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("api/Evidence")]
+        public Task<BaseResponse<Evidence>> InsertAsync([FromBody] Evidence model)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("api/Evidence")]
+        public Task<BaseResponse<Evidence>> UpdateAsync([FromBody] Evidence model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/IGenericApiController.cs
@@ -0,0 +1,19 @@
+﻿using PASVE.Models;
+using System.Web.Http;
+
+namespace PASVE.Interfaces
+{
+    public interface IGenericApiController<TEntity, TKey> where TEntity : class
+    {
+        [HttpGet]
+        Task<BaseResponse<List<TEntity>>> GetAllAsync(int page, int rows);
+        [HttpGet]
+        Task<BaseResponse<TEntity>> ConsultAsync([FromUri] TKey id);
+        [HttpPost]
+        Task<BaseResponse<TEntity>> InsertAsync([FromBody] TEntity model);
+        [HttpPut]
+        Task<BaseResponse<TEntity>> UpdateAsync([FromBody] TEntity model);
+        [HttpGet]
+        Task<BaseResponse<TEntity>> DeleteAsync([FromUri] TKey id);
+    }
+}
